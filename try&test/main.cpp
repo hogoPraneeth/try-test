72691ef445c33f0affe8da38aa8e8e4a1673ca55
Praneeth Patil <praneethpatil@Praneeths-MacBook-Pro.local>
2017-09-30 16:40:15 -0700

    Initial Commit

---
+++ try&test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  try&test
+//
+//  Created by Praneeth Patil on 9/30/17.
+//  Copyright © 2017 Praneeth Patil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
